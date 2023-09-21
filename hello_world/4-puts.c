7a52be8213cd9b3e1173afdff5eaf935a6d1597e
joshualewis0124 <7211@holbertonstudents.com>
2023-09-21 14:04:13 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+/*prints the line*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts( "\"Programming is like building a multilingual puzzle" );
 	return(0);
 }
