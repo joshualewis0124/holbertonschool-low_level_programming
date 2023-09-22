43e21c12c85bc4f5ecb6dbf8500edf524a73be40
joshualewis0124 <7211@holbertonstudents.com>
2023-09-22 14:56:57 -0700

    commit

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,12 @@
 int main(void)
 {
 	char i;
+
 	for (i = 'a' ; i <= 'z' ; i++)
-	      	putchar(i);
-	for (i = 'A' ; i <= 'Z' ; i++)
-		putchar(i);
-	putchar('\n');
-	return (0);
+	{
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	}
+		putchar('\n');
+		return (0);
 }
