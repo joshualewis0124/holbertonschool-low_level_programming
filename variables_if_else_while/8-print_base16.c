508a715977f6f78a3e77bfabdf8239f2bd9f361b
joshualewis0124 <7211@holbertonstudents.com>
2023-09-22 15:38:55 -0700

    commit

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,10 +7,13 @@
 */
 int main(void)
 {
-	char i;
+	int i;
+	char j;
 
-	for (i = 'a' ; i <= 'z' ; i++)
-		putchar(i);
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
