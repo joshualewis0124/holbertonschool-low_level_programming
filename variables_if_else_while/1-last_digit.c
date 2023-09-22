6b5043a865ba0e52c282edc6869cc92a3b361042
joshualewis0124 <7211@holbertonstudents.com>
2023-09-22 15:56:13 -0700

    commit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,6 +23,5 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	else if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	printf("\n");
 	return (0);
 }
