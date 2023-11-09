e60d6a1e0ed85bb2c99df5eb5503aab993233d60
Joshualewis0124 <7211@holbertonstudents.com>
2023-11-09 09:53:56 -0800

    print list fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,12 +4,11 @@
  * print_list - prints all elements of a list
  * @h: pointer to the list_t list to print
  *
- * Return - number of nodes printed
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
-	
 	while (h)
 	{
 		if (!h->str)
